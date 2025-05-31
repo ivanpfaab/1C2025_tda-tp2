8f896b0625f7c2f4ec93409ed184bd85f4599268
FGazcon <fgazcon@fi.uba.ar>
2025-05-31 01:02:58 -0300

    'Finalizado' parte 3, imprime las conecciones, E-K

--- red_WAN.py
+++ red_WAN.py
@@ -1,8 +1,28 @@
 import networkx as nx
+from networkx.algorithms.flow import edmonds_karp
 import matplotlib.pyplot as plt
 
 def WAN_network(D, b, k, d):
-    create_graph(D, b, k, d)
+    network = create_graph(D, b, k, d)
+    
+    #La libreria NetworkX de python provee un algoritmo Edmonds_Karp que nos da el 
+    #Flujo maximo y un diccionario con los caminos resultantes.
+    flujo_maximo, residual_ek = nx.maximum_flow(network, 'S', 'T', 'capacity', edmonds_karp)
+
+    if flujo_maximo < len(d[0]) * k:
+        print("Numero minimo de backups inalcanzable")
+        return
+    else:
+        print_connections(residual_ek)
+    
+    return residual_ek
+
+def print_connections(residual_ek):
+    for u in residual_ek:
+        for v in residual_ek[u]:
+            flow = residual_ek[u][v]
+            if flow:
+                print(f"{u} → {v}: {flow}")
 
 def create_graph(D, b, k, d):
     newGraph = nx.DiGraph()
@@ -21,20 +41,26 @@ def create_graph(D, b, k, d):
             if d[i][j] < D and i != j:
                 newGraph.add_edge(f"{i}_IN", f"{j}_OUT", capacity = 1)
 
+    show_graph(newGraph)
 
-    
+    return newGraph
+
+def show_graph(newGraph):
+
     # DIBUJO DEL GRAFO 
     pos = {}
     spacing = 1.5
 
+    n = int((len(newGraph.nodes) - 2) / 2)
+
     pos['S'] = (0, -spacing)
     pos['T'] = (3, -spacing)
 
-    for i in range(len(d)):
+    for i in range(n):
         pos[f"{i}_IN"] = (1, -i * spacing)
         pos[f"{i}_OUT"] = (2, -i * spacing)
 
-    plt.figure(figsize=(10, len(d))) 
+    plt.figure(figsize=(10, n)) 
     nx.draw(newGraph, pos, with_labels=True, node_size=1000, node_color="lightblue", arrowsize=20)
     edge_labels = nx.get_edge_attributes(newGraph, 'capacity')
     nx.draw_networkx_edge_labels(newGraph, pos, edge_labels=edge_labels)
@@ -42,15 +68,15 @@ def create_graph(D, b, k, d):
     plt.tight_layout()
     plt.show()
 
-    return newGraph
-
 def main():
-    WAN_network(6, 2, 5, [
+    #D, b, k, d
+    #Distancia max, limite antenas, minimo antenas
+    WAN_network(5, 2, 1, [
         [1, 2, 3, 4, 5],
         [1, 2, 3, 4, 5],
-        [1, 2, 3, 4, 5], 
-        [1, 2, 3, 4, 5], 
-        [1, 2, 3, 4, 5]
+        [1, 2, 3, 4, 1], 
+        [1, 2, 3, 4, 1], 
+        [5, 5, 5, 4, 5]
     ])
 
 main()
